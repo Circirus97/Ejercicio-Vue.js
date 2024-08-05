f0fed36c158d1219606d917f5375c38a9c6f65d1
Circirus97 <msuarezalejandra@gmail.com>
2024-08-05 08:14:38 -0500

    Create banner

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <h1 class="p-100">Hola mundo en VUE.JS</h1>
 
-  <p>Contador: {{ counter }}</p>
+  <Banner />
 
-  <button @click="increment" >Incrementa +</button>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 
- 
-  const counter = ref<number>(1)
-
-  const increment = () => {
-    counter.value ++
-  }
+import Banner from '@/components/Banner.vue'
 
 </script>
 
 <style>
-h1{
-  color: red;
-}
+
 </style>
 
---
+++ src/components/Banner.vue
@@ -0,0 +1,76 @@
+<template>
+
+    <section class="banner-section w-100 p-10">
+        <form>
+            <div class="search-input d-middle">
+                <input 
+                type="text"
+                required="true"
+                >
+
+                <span>Search movie</span>
+
+            </div>
+
+        </form>
+
+        <div class="container-banner-img">
+            <!--Se le agg : al inicio del src para que tome lo que vaa  estar en la etiqueta o tambien como v-bind -->
+            <img :src="img" alt="">
+        </div>
+    </section>
+
+</template>
+
+<script setup lang="ts">
+// @ts-ignore //deshabilita TypeScript solo en la siguiente linea
+import svgImage from '@/assets/images/header-img.svg'
+
+const img = svgImage;
+
+</script>
+
+<style lang="scss" scoped>  // el atributo scoped es para que los estilos solo me apliquen en esta etiqueta template
+
+.banner-section{
+    width: 100%;
+    height: 50vh;
+    background-image: url(../assets/images/banner-bg.png);
+    background-size: cover;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    
+    
+
+    //Selectores anidados
+    form{
+        .search-input{
+
+        }
+    }
+
+    .container-banner-img img{
+        width: 200px;
+        animation: animated-img 4s linear infinite;
+    }
+}
+
+.d-middle{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+@keyframes animated-img{
+    30%{
+        transform: translateY(-15px);
+    }
+    60%{
+        transform: translateY(30%px);
+    }
+}
+
+
+
+</style>
